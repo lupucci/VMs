baf8471954757a2226450c0db0b41219ca86c283
Luiz Pucci <lupucci05@gmail.com>
2025-03-18 17:19:54 -0300

    Subindo VMs linux e windows

---
+++ VMLINUX/vmlinux.tf
@@ -0,0 +1,112 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "99c659de-a375-48a0-a696-1bcaca8802ce"
+
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-linux"
+  location = "brazilsouth"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-linux"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16", "192.16.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-vmlinux"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "publicip" {
+  name                    = "publicip-vmlinux-luiz"
+  resource_group_name     = azurerm_resource_group.rg.name
+  location                = azurerm_resource_group.rg.location
+  allocation_method       = "Dynamic"
+  sku                     = "Basic"
+  idle_timeout_in_minutes = 30
+  domain_name_label       = "vminserverluiz"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-vmlinux"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  ip_configuration {
+    name                          = "ipconfig-vmlinux"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.publicip.id
+  }
+
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-vmlinux"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+}
+
+variable "regras_entrada" {
+  type = map(any)
+  default = {
+    101 = 80
+    102 = 443
+    103 = 3389
+    104 = 22
+  }
+}
+
+resource "azurerm_network_security_rule" "nsgrule" {
+  for_each                    = var.regras_entrada
+  name                        = "nsgrule-${each.key}"
+  priority                    = each.key
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = each.value
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+
+}
+
+resource "azurerm_network_interface_security_group_association" "nsgassociation" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_linux_virtual_machine" "vmlinux" {
+  name                  = "vmlinux-luiz"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = "Standard_B1s"
+  admin_username        = "luiz"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  admin_ssh_key {
+    username   = "luiz"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+}
---
+++ VMWINDOWS/virtualmachine.tf
@@ -0,0 +1,118 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "99c659de-a375-48a0-a696-1bcaca8802ce"
+
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-vmwindows"
+  location = "brazilsouth"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-vmwindows"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16", "192.16.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-vmwindows"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "publicip" {
+  name                    = "publicip-vmwindows-luiz"
+  resource_group_name     = azurerm_resource_group.rg.name
+  location                = azurerm_resource_group.rg.location
+  allocation_method       = "Dynamic"
+  sku                     = "Basic"
+  idle_timeout_in_minutes = 30
+  domain_name_label       = "vminserverluiz"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-vmwindows"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  ip_configuration {
+    name                          = "ipconfig-vmwindows"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.publicip.id
+  }
+
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-vmwindows"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+}
+
+variable "regras_entrada" {
+  type = map(any)
+  default = {
+    101 = 80
+    102 = 443
+    103 = 3389
+    104 = 22
+  }
+}
+
+resource "azurerm_network_security_rule" "nsgrule" {
+  for_each                    = var.regras_entrada
+  name                        = "nsgrule-${each.key}"
+  priority                    = each.key
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = each.value
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+
+}
+
+resource "azurerm_network_interface_security_group_association" "nsgassociation" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "vm-vmwindows"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_B1s"
+
+  os_profile {
+    computer_name  = "vmwindows"
+    admin_username = "adminuser"
+    admin_password = "Password1234!"
+  }
+
+  storage_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "osdisk-vmwindows"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile_windows_config {
+    provision_vm_agent = true
+  }
+}
